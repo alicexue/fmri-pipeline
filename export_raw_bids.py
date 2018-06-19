f2d6feb08d41f9ce1538a14ada6acba59df69663
Alice Xue <alice.m.xue@gmail.com>
2018-06-19 15:22:38 -0400

    Added fw cli check - needed for BIDS export

--- export_raw_bids.py
+++ export_raw_bids.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 """
-Exports raw BIDS from flywheel for each subject 
+Exports raw BIDS from flywheel for each subject into a directory called 'raw'
 Only exports raw BIDS for subjects whose fmriprep directories exist
 Note: raw BIDS can only be exported if docker is running
 """
 
 # Created by Alice Xue, 06/2018
 
-import subprocess as sp
 import os
+import subprocess as sp
 import sys
+
 from directory_struct_utils import *
 
 def main():
@@ -27,6 +28,7 @@ def export_raw_bids(studyid,basedir,project_label):
 	if not os.path.exists(rawdir):
 		os.makedirs(rawdir)
 
+	# checks whether docker is installed (but not if docker is running)
 	dockerExists=False
 	try:
 		FNULL = open(os.devnull, 'w')
@@ -35,25 +37,43 @@ def export_raw_bids(studyid,basedir,project_label):
 	except:
 		dockerExists=False
 
+	fwExists=False	
+	try:	
+		FNULL = open(os.devnull, 'w')	
+		sp.call(['fw'],stdout=FNULL, stderr=sp.STDOUT)	
+		fwExists=True	
+	except:	
+		fwExists=False
+
 	print '\n## Exporting raw BIDS now ##\n'
 
-	if dockerExists:
+	if fwExists and dockerExists:
+		# gets all subjects with fmriprep directories
 		all_subs=get_all_subs(studydir)
-		for sub in all_subs:
+		for sub in all_subs: # sub is missing the prefix 'sub-'
 			rawsubdir=os.path.join(rawdir,'sub-%s'%sub)
 			if os.path.exists(rawsubdir):
 				print "Skipping download of raw bids for sub-%s"%sub
 			else:
+				# creates tmp dir specific to the subject
 				tmpsubdir=os.path.join(studydir,'tmp_sub-%s_BIDS'%sub)
 				os.mkdir(tmpsubdir)
+				# call subprocess to export BIDS
 				commands=['fw','export','bids',tmpsubdir,'-p',project_label,'--subject',sub]
 				print 'Calling:',' '.join(commands)
 				sp.call(commands)
+				# moves the exported directory into the raw directory
 				rawsubout=os.path.join(tmpsubdir,'sub-%s'%sub)
 				print 'Moving %s to %s'%(rawsubout,rawdir)
 				sp.call(['mv',rawsubout,rawdir])
+				# removes tmp directory that was created
 				print 'Removing %s'%(tmpsubdir)
 				sp.call(['rm','-rf',tmpsubdir])
+	else:
+		if not fwExists:
+			print 'Flywheel CLI is not installed, which is required for exporting raw BIDS.'
+		if not dockerExists:
+			print 'Docker is not installed. Docker is required to export raw BIDS from flywheel'
 
 if __name__ == '__main__':
     main()
--- manage_flywheel_downloads.py
+++ manage_flywheel_downloads.py
@@ -5,14 +5,15 @@ Takes command line input and runs download_flywheel_fmriprep and export_raw_bids
 
 # Created by Alice Xue, 06/2018
 
+import flywheel
+import os
 import subprocess as sp
 import sys
-import os
-import flywheel
+
 import download_flywheel_fmriprep
 import export_raw_bids
 
-
+# checks if docker is installed
 dockerExists=False
 try:
 	FNULL = open(os.devnull, 'w')
@@ -21,14 +22,21 @@ try:
 except:
 	dockerExists=False
 
-APIKeyFile='flywheel_API_key.txt'
+fwExists=False	
+try:	
+	FNULL = open(os.devnull, 'w')	
+	sp.call(['fw'],stdout=FNULL, stderr=sp.STDOUT)	
+	fwExists=True	
+except:	
+	fwExists=False
 
+APIKeyFile='flywheel_API_key.txt'
+# checks for existing APIKeyFile and uses the key stored there
 if os.path.exists(APIKeyFile):
 	with open(APIKeyFile,'r') as f:
 		key = f.readline()
 		fw = flywheel.Flywheel(key) 
-		print key
-else:
+else: # asks for the key from the command line
 	loggedIn=False
 	while not loggedIn:
 		key=raw_input('Enter Your Flywheel API Key: ')
@@ -38,7 +46,7 @@ else:
 		except:
 			print 'Invalid API key.'
 	print 'Your API key will now be saved as %s'%APIKeyFile
-	print 'Don\'t put this on GitHub!
+	print 'Don\'t put this on GitHub!'
 	with open(APIKeyFile,'w') as f:
 		f.write(key)
 
@@ -46,36 +54,39 @@ self = fw.get_current_user()
 print 'You can ignore the UserWarning.'
 print('\nYou are now logged in as %s %s.' % (self.firstname, self.lastname))
 
-groups=fw.get_all_groups()
-
+# Ask for group id
 print ''
+groups=fw.get_all_groups()
 if len(groups)>0:
 	print 'Here are your groups:'
 	for group in fw.get_all_groups():
 		print('%s: %s' % (group.id, group.label))
 group_id=raw_input('Enter the group id: ')
 
+# Ask for project label
 print ''
 projects=fw.get_group_projects(group_id)
 if len(projects)>0:
+	# lists projects in this group the user has access to
 	print 'Here are your projects:'
 	project_labels=[]
 	for project in projects:
 		project_id=project.id
 		project_labels.append(project.label)
 		print('Project label: %s' % (project.label))
+	# asks for project label
 	my_project=''
 	my_project=raw_input('Enter the project label: ')
 	while my_project not in project_labels:
-		print "HERE",my_project,project_labels
 		print '\nInvalid project label. See list of project labels above'
 		my_project=raw_input('Enter the project label: ')
 	project_label=my_project
-else:
+else: # if no projects are found, asks to enter group id again
 	while len(fw.get_group_projects(group_id))==0:
 		print 'No projects found in group %s.'%(group_id)
 		print 'Are you sure the group id is %s?'%(group_id)
 		group_id=raw_input('Enter the group id: ')
+	# lists projects
 	print '\nHere are your projects:'
 	projects=fw.get_group_projects(group_id)
 	project_labels=[]
@@ -86,16 +97,17 @@ else:
 	my_project=''
 	my_project=raw_input('Enter the project label: ')
 	while my_project not in project_labels:
-		print "HERE",my_project, project_labels
 		print '\nInvalid project label. See list of project labels above'
 		my_project=raw_input('Enter the project label: ')
 	project_label=my_project
 	
-
+# Asks for study id
 print '\nThe study id is the name of the directory that the fmriprep output will be downloaded to.'
 studyid=raw_input('Enter the study id: ')
+# Asks for base directory
 print '\nThe base directory is the full path of where the study id directory will be created. (Don\'t include study id here.)'
 basedir=raw_input('Enter the base directory: ')
+# Checks if base directory exists and asks again if it doesn't
 while not os.path.exists(basedir):
 	print 'Invalid base directory.'
 	basedir=raw_input('Enter the base directory: ')
@@ -106,6 +118,7 @@ if not os.path.exists(studydir):
 else:
 	print '\nNote: %s already exists. Only the remaining subjects\' data will be downloaded.'%studydir
 
+# Asks if user wants to export BIDS, which is only allowed if docker is installed and running
 exportRawBids=False
 if dockerExists:
 	rsp=''
@@ -120,9 +133,13 @@ if dockerExists:
 			if rsp2=='n':
 				print 'Since docker is not running, raw BIDS won\'t be exported.'
 				exportRawBids=False
+			elif rsp2=='y' and not fwExists: 
+				print 'Flywheel CLI not installed, which is necessary for exporting raw BIDS.'
 else:
 	print 'Note: docker is not installed. Raw BIDS cannot be exported.'
 
+# downloads fmriprep outputs 
 download_flywheel_fmriprep.download_flywheel_fmriprep(key,group_id,project_label,studyid,basedir)
+# exports raw BIDS (export of raw BIDS should happen after downloading fmriprep output because export_raw_bids looks for subjects in fmriprep folder)
 if exportRawBids:
 	export_raw_bids.export_raw_bids(studyid,basedir,project_label)
